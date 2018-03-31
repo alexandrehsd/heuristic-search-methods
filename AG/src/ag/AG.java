51f2c0cc9d7410df307e7044f0f6db5737a06f99
Alexandre Henrique <alexandre.hsd@gmail.com>
2018-03-31 11:52:08 -0300

    fitness function added

--- AG/src/ag/AG.java
+++ AG/src/ag/AG.java
@@ -1,53 +1,85 @@
 
 package ag;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class AG {
     public static void main(String[] args) {
+        System.out.printf("Descreva com um inteiro de 1 a 5 o quanto você leva em conta\nas avaliações "
+                + "dadas pelos outros usuários sobre as academias: ");
         
-        int N = 20; //Número de indivíduos
-        Chromosome[] pop = new Chromosome[N]; //Vetor de indivíduos -> população
+        Scanner input = new Scanner(System.in);
         
-        //Inicializando cada cromossomo da população de cromossomos
-        for(int i=0;i<pop.length;i++){
-            pop[i] = new Chromosome();
-        }
+        //Entrando com a variável que determinará o peso das avaliações na função fitness
+        int rweight = input.nextInt();
+        Chromosome.setCr(rweight);
         
-        //vetores auxiliares para seleção e crossover
-        Chromosome[] selected;
-        Chromosome[] sons;
-         
-        //ordenando a população para definir quem são os 2 melhores indivíduos
-        //que passarão para a próxima geração
-        Sort.BubbleSort(pop);
+        System.out.printf("\nPor favor, indique a faixa de preços que você estaria disposto a pagar\n"
+                + "mensalmente, entrando com o valor máximo e o mínimo, respectivamente.\nMax: ");
         
-        //variável responsável por limitar o número de iterações
-        int iterations = 0;
-        //Variável randômica utilizada para selecionar o indivíduo que sofrerá mutação 
-        Random rand = new Random();
-        while(iterations < 40){
-            //Seleciona os indivíduos para crossover, exceto os 2 melhores indivíduos
-            selected = Operator.selection(pop, pop.length-2);
-            
-            //Cruza os indivíduos que foram selecionados
-            sons = Operator.crossover(selected);
-            
-            //ordenando a população para substituir os N-2 piores indivíduos
-            //pelos novos indivíduos que foram criados no crossover
-            Sort.BubbleSort(pop);
-            
-            //substituindo os N-2 indivíduos piores pelos N-2 gerados
-            System.arraycopy(sons, 0, pop, 0, pop.length-2);
-            
-            //Aplicando mutação a um individuo aleatório entre
-            //tendo o cuidado de não selecionar os individuos 18 e 19
-            //do vetor de população, que têm os melhores fitness
-            Operator.mutation(pop[rand.nextInt(19)]);
-            
-            iterations++;
-        }
+        //Entrando com as variáveis que vão determinar Cp na função de avaliação
+        int maxp = input.nextInt();
+        System.out.printf("Min: ");
+        int minp = input.nextInt();
+        Chromosome.setCp(maxp, minp);
         
+        
+//        
+//        
+//        int N = 20; //Número de indivíduos
+//        Chromosome[] pop = new Chromosome[N]; //Vetor de indivíduos -> população
+//       
+//        //Inicializando cada cromossomo da população de cromossomos
+//        for(int i=0;i<pop.length;i++){
+//            pop[i] = new Chromosome();
+//        }
+        
+//        //Mostrando alguns cromossomos
+//        pop[0].show();
+//        pop[1].show();
+//        pop[2].show();
+//        //Mostrando o fitness de cada um
+//        System.out.println(pop[0].fitness());
+//        System.out.println(pop[1].fitness());
+//        System.out.println(pop[2].fitness());
+
+//        
+//        //vetores auxiliares para seleção e crossover
+//        Chromosome[] selected;
+//        Chromosome[] sons;
+//         
+//        //ordenando a população para definir quem são os 2 melhores indivíduos
+//        //que passarão para a próxima geração
+//        Sort.BubbleSort(pop);
+//        
+//        //variável responsável por limitar o número de iterações
+//        int iterations = 0;
+//        //Variável randômica utilizada para selecionar o indivíduo que sofrerá mutação 
+//        Random rand = new Random();
+//        while(iterations < 40){
+//            //Seleciona os indivíduos para crossover, exceto os 2 melhores indivíduos
+//            selected = Operator.selection(pop, pop.length-2);
+//            
+//            //Cruza os indivíduos que foram selecionados
+//            sons = Operator.crossover(selected);
+//            
+//            //ordenando a população para substituir os N-2 piores indivíduos
+//            //pelos novos indivíduos que foram criados no crossover
+//            Sort.BubbleSort(pop);
+//            
+//            //substituindo os N-2 indivíduos piores pelos N-2 gerados
+//            System.arraycopy(sons, 0, pop, 0, pop.length-2);
+//            
+//            //Aplicando mutação a um individuo aleatório entre
+//            //tendo o cuidado de não selecionar os individuos 18 e 19
+//            //do vetor de população, que têm os melhores fitness
+//            Operator.mutation(pop[rand.nextInt(19)]);
+//            
+//            iterations++;
+//        }
+
     }
-    
 }
+    
--- AG/src/ag/Chromosome.java
+++ AG/src/ag/Chromosome.java
@@ -1,10 +1,16 @@
 package ag;
 
+import java.util.Arrays;
 import java.util.Random;
+import static java.lang.Math.pow;
 
 public class Chromosome {
     private int[] GenCode = new int[12];
     private double fitness;
+    
+    //Variável da classe, não vinculadas a nenhum cromossomo em particular
+    //Constantes definidas com base na entrada do usuário
+    private static double Cr=1, Cp=1;
     
     //Inicializando os cromossomos como um vetor de bits randômicos
     Chromosome(){
@@ -12,12 +18,6 @@ public class Chromosome {
         for(int i = 0; i<12; i++){
             this.GenCode[i] = rand.nextInt(2);
         }
-    }
-     
-    //Método destinado à função de avaliação -> não implementado
-    public double fitness(){
-        Random rand = new Random();
-        return rand.nextInt(11);
     }
     
     //Configura um bit no código genético -> Mutação
@@ -53,10 +53,208 @@ public class Chromosome {
     
     //Mostra o código genético de um indivíduo
     public void show(){
-        for(int i =0;i<12;i++){
-            System.out.printf("%d",this.GenCode[i]);
+        System.out.println(Arrays.toString(this.GenCode).replaceAll("\\[|\\]|,|\\s", ""));
+    }
+    
+    public static void setCr(int rateweight){
+        switch(rateweight){
+            case 1:
+                Chromosome.Cr = 0.5;
+                break;
+            case 2:
+                Chromosome.Cr = 1.0;
+                break;
+            case 3:
+                Chromosome.Cr = 1.5;
+                break;
+            case 4:
+                Chromosome.Cr = 2.0;
+                break;
+            case 5:
+                Chromosome.Cr = 2.5;
+                break;
+            default:
+                System.out.println("Esta entrada é inválida!");
         }
     }
     
+    public static void setCp(int maxp, int minp){
+        if (maxp - minp < 50 && maxp < 120){
+            Chromosome.Cp = 2.5;
+        } else if (maxp - minp >= 50 && maxp < 250){
+            Chromosome.Cp = 1.5;
+        } else if (maxp >= 250){
+            Chromosome.Cp = 1.25;
+        }
+    }
+    //Método destinado à função de avaliação -> não implementado
+    public double fitness(){
+        double d, h, r, p;
+        //the gencode has a form [ddddrrrrpphh]
+        //d stands for distance, r for rate, p for price and h for hour.
+        
+        d = this.setd(this.getBits(0, 3));
+        r = this.setr(this.getBits(4, 7));
+        p = this.setp(this.getBits(8, 9));
+        h = this.seth(this.getBits(10, 11));
+        
+        double value;
+        value = 7/pow(d,0.75) + pow(h,2) +Chromosome.Cr*pow(r,1.5) + Chromosome.Cp*10/p;
+        return value;
+    }
+    
+    public double setd(int bits[]){
+        double d=2;
+        switch(Arrays.toString(bits).replaceAll("\\[|\\]|,|\\s", "")){
+            case "0000":
+                d = 0.25;
+                break;
+            case "0001":
+                d = 0.5;
+                break;
+            case "0010":
+                d = 0.75;
+                break;
+            case "0011":
+                d = 1.0;
+                break;
+            case "0100":
+                d = 1.25;
+                break;
+            case "0101":
+                d = 1.50;
+                break;
+            case "0110":
+                d = 1.75;
+                break;
+            case "0111":
+                d = 2.0;
+                break;
+            case "1000":
+                d = 2.25;
+                break;
+            case "1001":
+                d = 2.50;
+                break;
+            case "1010":
+                d = 2.75;
+                break;
+            case "1011":
+                d = 3.0;
+                break;
+            case "1100":
+                d = 3.25;
+                break;
+            case "1101":
+                d = 3.50;
+                break;
+            case "1110":
+                d = 3.75;
+                break;
+            case "1111":
+                d = 4.0;
+                break;
+            default:
+                System.out.println("Esta string de distância é inválida");
+        }
+        return d;
+    }
+    
+    public double setr(int bits[]){
+        double r=2;
+        switch(Arrays.toString(bits).replaceAll("\\[|\\]|,|\\s", "")){
+            case "0000":
+                r = 0.25;
+                break;
+            case "0001":
+                r = 0.5;
+                break;
+            case "0010":
+                r = 0.75;
+                break;
+            case "0011":
+                r = 1.0;
+                break;
+            case "0100":
+                r = 1.25;
+                break;
+            case "0101":
+                r = 1.50;
+                break;
+            case "0110":
+                r = 1.75;
+                break;
+            case "0111":
+                r = 2.0;
+                break;
+            case "1000":
+                r = 2.25;
+                break;
+            case "1001":
+                r = 2.50;
+                break;
+            case "1010":
+                r = 2.75;
+                break;
+            case "1011":
+                r = 3.0;
+                break;
+            case "1100":
+                r = 3.25;
+                break;
+            case "1101":
+                r = 3.50;
+                break;
+            case "1110":
+                r = 3.75;
+                break;
+            case "1111":
+                r = 4.0;
+                break;
+            default:
+                System.out.println("Esta string de rating é inválida");
+        }
+        return r;
+    }
+    
+    public double setp(int bits[]){
+        double p=1;
+        switch(Arrays.toString(bits).replaceAll("\\[|\\]|,|\\s", "")){
+            case "00": p = 1.3;
+                break;
+            case "01":
+                p = 1.7;
+                break;
+            case "10":
+                p = 2.0;
+                break;
+            case "11":
+                p = 4.0;
+                break;
+            default:
+                System.out.println("Esta string de preço é inválida");
+        }
+        return p;
+    }
+    public double seth(int bits[]){
+        double h=1;
+        switch(Arrays.toString(bits).replaceAll("\\[|\\]|,|\\s", "")){
+            case "00":
+                h = 0.5;
+                break;
+            case "01":
+                h = 2.0;
+                break;
+            case "10":
+                h = 2.0;
+                break;
+            case "11":
+                h = 4.0;
+                break;
+            default:
+                System.out.println("Esta string de horário é inválida");
+        }
+        return h;
+    }
     
 }
