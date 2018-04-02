84d6e41cb3b3c86543ce17abb5cc895b3abd8633
gabrielmcoelho <gabriel_mcoelho@hotmail.com>
2018-04-02 02:50:19 -0300

    Update AG.java

--- AG/src/ag/AG.java
+++ AG/src/ag/AG.java
@@ -1,4 +1,3 @@
-
 package ag;
 
 import java.io.IOException;
@@ -14,82 +13,27 @@ import org.json.JSONException;
 //18 não tem rating
 public class AG {
     public static void main(String[] args) throws IOException, MalformedURLException, JSONException {
-//        String addr = "419+Rua+Mipibu";
-//        String lat = "-5.8158596";
-//        String lng = "-35.2091204";
-//        System.out.println(GymBuilder.distance(lat, lng, addr));
-//          GymBuilder.HourEncode("05h00", "20h00", "16h00", "19h00", "ChIJvexClv__sgcRXCamTA6EXJc");
-//          GymBuilder.DistEncode(1899);
-//        System.out.printf("Descreva com um inteiro de 1 a 5 o quanto você leva em conta\nas avaliações "
-//                + "dadas pelos outros usuários sobre as academias: ");
-//        
-//        Scanner input = new Scanner(System.in);
-//        
-//        //Entrando com a variável que determinará o peso das avaliações na função fitness
-//        int rweight = input.nextInt();
-//        Chromosome.setCr(rweight);
-//        
-//        System.out.printf("\nPor favor, indique o preço máximo que você está disposto a pagar: ");
-//        
-//        //Entrando com as variáveis que vão determinar Cp na função de avaliação
-//        int maxp = input.nextInt();
-//        Chromosome.setCp(maxp);
-//        
+        // INICIALIZANDO AS VARIÁVEIS
+        Chromosome[] pop = new Chromosome[20];
+        Chromosome[] sons = new Chromosome[18];
+        Chromosome[] elites = new Chromosome[2];
+        GymBuilder.Gyms("Rua Alexandrino de Alencar, 1618", "18h00","22h00" , "13h00", "15h00"); // Gera database
+        // GERANDO A POPULAÇÃO INICIAL ALEATORIAMENTE
+        for(int i=0; i<20; i++){
+            pop[i] = new Chromosome();
+        }
+        // REALIZANDO O CRUZAMENTO E PRODUZINDO FILHOS
+        sons = Operator.crossover(Operator.selection(pop));
         
-//        
-//        
-//        int N = 20; //Número de indivíduos
-//        Chromosome[] pop = new Chromosome[N]; //Vetor de indivíduos -> população
-//       
-//        //Inicializando cada cromossomo da população de cromossomos
-//        for(int i=0;i<pop.length;i++){
-//            pop[i] = new Chromosome();
-//        }
-        
-//        //Mostrando alguns cromossomos
-//        pop[0].show();
-//        pop[1].show();
-//        pop[2].show();
-//        //Mostrando o fitness de cada um
-//        System.out.println(pop[0].fitness());
-//        System.out.println(pop[1].fitness());
-//        System.out.println(pop[2].fitness());
-
-//        
-//        //vetores auxiliares para seleção e crossover
-//        Chromosome[] selected;
-//        Chromosome[] sons;
-//         
-//        //ordenando a população para definir quem são os 2 melhores indivíduos
-//        //que passarão para a próxima geração
-//        Sort.BubbleSort(pop);
-//        
-//        //variável responsável por limitar o número de iterações
-//        int iterations = 0;
-//        //Variável randômica utilizada para selecionar o indivíduo que sofrerá mutação 
-//        Random rand = new Random();
-//        while(iterations < 40){
-//            //Seleciona os indivíduos para crossover, exceto os 2 melhores indivíduos
-//            selected = Operator.selection(pop, pop.length-2);
-//            
-//            //Cruza os indivíduos que foram selecionados
-//            sons = Operator.crossover(selected);
-//            
-//            //ordenando a população para substituir os N-2 piores indivíduos
-//            //pelos novos indivíduos que foram criados no crossover
-//            Sort.BubbleSort(pop);
-//            
-//            //substituindo os N-2 indivíduos piores pelos N-2 gerados
-//            System.arraycopy(sons, 0, pop, 0, pop.length-2);
-//            
-//            //Aplicando mutação a um individuo aleatório entre
-//            //tendo o cuidado de não selecionar os individuos 18 e 19
-//            //do vetor de população, que têm os melhores fitness
-//            Operator.mutation(pop[rand.nextInt(19)]);
-//            
-//            iterations++;
-//        }
-
+        // OPERAÇÃO DE ELITISMO
+        Sort.BubbleSort(pop); // Ordena o vetor de população antiga de modo que os E indivíduos com maior fitness fiquem no final do vetor
+        // REALIZANDO MUTAÇÃO
+        Random rand = new Random();
+        int index = rand.nextInt(18);
+        sons[index] = Operator.mutation(sons[index]); // Seleciona um dos filhos gerados para aplicar a mutação
+        // SUBSTITUINDO A GERAÇÃO ANTIGA PELA NOVA GERAÇÃO
+        for(int i=0; i<18; i++){ // Substitui a população pelos novos filhos
+            pop[i] = sons[i]; 
+        }
     }
 }
-    
